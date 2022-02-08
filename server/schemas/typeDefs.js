f4c97b873d47a0e6ff024c45feb9759570ce4016
trujilml <mickeytrj@gmail.com>
2022-02-08 10:15:18 -0600

    created the thought reaction type def and implemented them in the resolver - played with them in graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,7 +4,21 @@ const {gql} = require('apollo-server-express');
 //create our typeDefs
 const typeDefs = gql`
 type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
+}
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+}
+type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
 }
 `;
 
