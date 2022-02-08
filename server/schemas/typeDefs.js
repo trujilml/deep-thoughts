86dd7ca4c26ab542018ffeb7cb2f2f80499f3302
trujilml <mickeytrj@gmail.com>
2022-02-08 12:42:59 -0600

    constructed the remaining query type definitions and resolvers - played around in graphql playground

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,19 @@ const {gql} = require('apollo-server-express');
 
 //create our typeDefs
 const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+}
 type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
 }
 type Thought {
     _id: ID
