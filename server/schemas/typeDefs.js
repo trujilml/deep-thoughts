e92bf270c36c96e674aec916107b9334ba4d6173
trujilml <mickeytrj@gmail.com>
2022-02-09 20:02:17 -0600

    repositioned auth file to work properly - now all mutations can be sent

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,49 +1,51 @@
-//import gql tagged template function
-const {gql} = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
-//create our typeDefs
 const typeDefs = gql`
-type Auth {
-    token: ID!
-    user: User
-}
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     friendCount: Int
     thoughts: [Thought]
     friends: [User]
-}
-type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
-}
-type Thought {
+  }
+
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
     username: String
     reactionCount: Int
     reactions: [Reaction]
-}
-type Reaction {
+  }
+
+  type Reaction {
     _id: ID
     reactionBody: String
     createdAt: String
     username: String
-}
-type Mutation {
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     addThought(thoughtText: String!): Thought
     addReaction(thoughtId: ID!, reactionBody: String!): Thought
     addFriend(friendId: ID!): User
-}
+  }
 `;
 
-//export typeDefs
 module.exports = typeDefs;
