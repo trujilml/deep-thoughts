ea602ff19e69bf47be9c83ff0f9754fe3fe3cd81
trujilml <mickeytrj@gmail.com>
2022-02-12 23:09:14 -0600

    created the thoughtlist component and relaunched server to show the application applying data including graphql launch of two servers

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '/@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
