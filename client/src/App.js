83f624d620cff15ebdc6035ae625649d59d5be5c
trujilml <mickeytrj@gmail.com>
2022-02-12 23:42:48 -0600

    set up the main url routes using react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import Home from './pages/Home';
 
@@ -17,13 +24,21 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
     <div className='flex-column justify-flex-start min-100-vh'>
       <Header />
       <div className='container'>
-        <Home />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/thought" component={SingleThought} />
+
+      <Route component={NoMatch} />
       </div>
       <Footer />
     </div>
+    </Router>
     </ApolloProvider>
   );
 }
