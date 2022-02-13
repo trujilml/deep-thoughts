4fd6a6556588bde36d1045f987df85b37bfff794
trujilml <mickeytrj@gmail.com>
2022-02-13 12:03:24 -0600

    built the profile page and implemented it in gql

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -35,3 +35,24 @@ query thought($id: ID!) {
     }
 }
 `;
+
+export const QUERY_USER = gql`
+query user($username: String!) {
+    user(username: $username) {
+        _id
+        username
+        email
+        friendCount
+        friends {
+            _id
+            username
+        }
+        thoughts {
+            _id
+            thoughtText
+            createdAt
+            reactionCount
+        }
+    }
+}
+`;
