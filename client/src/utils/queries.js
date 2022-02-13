e53417e55ff8f70b417ec6e2ed9d09906c7966e3
trujilml <mickeytrj@gmail.com>
2022-02-13 11:36:41 -0600

    built the single thought page and created a reaction list with the addition of a single thought query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -17,3 +17,21 @@ query thoughts($username: String) {
     }
 }
 `;
+
+export const QUERY_THOUGHT = gql`
+query thought($id: ID!) {
+    thought(_id: $id) {
+        _id
+        thoughtText
+        createdAt
+        username
+        reactionCount
+        reactions {
+            _id
+            createdAt
+            username
+            reactionBody
+        }
+    }
+}
+`;
