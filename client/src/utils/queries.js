f126bbce3e5cf5d57c1ee18127e006269204b4b2
trujilml <mickeytrj@gmail.com>
2022-02-14 21:10:05 -0600

    created and implemented user based queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,58 +1,100 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_THOUGHTS = gql`
-query thoughts($username: String) {
+  query thoughts($username: String) {
     thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
         _id
-        thoughtText
         createdAt
         username
-        reactionCount
-        reactions {
-            _id
-            createdAt
-            username
-            reactionBody
-        }
+        reactionBody
+      }
     }
-}
+  }
 `;
 
 export const QUERY_THOUGHT = gql`
-query thought($id: ID!) {
+  query thought($id: ID!) {
     thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
         _id
-        thoughtText
         createdAt
         username
-        reactionCount
-        reactions {
-            _id
-            createdAt
-            username
-            reactionBody
-        }
+        reactionBody
+      }
     }
-}
+  }
 `;
 
 export const QUERY_USER = gql`
-query user($username: String!) {
+  query user($username: String!) {
     user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
         _id
         username
-        email
-        friendCount
-        friends {
-            _id
-            username
-        }
-        thoughts {
-            _id
-            thoughtText
-            createdAt
-            reactionCount
-        }
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
     }
-}
+  }
+`;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          reactionBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
 `;
