47287947ed6bf49321c308dfb63eb336bb5ece75
trujilml <mickeytrj@gmail.com>
2022-02-15 20:01:25 -0600

    updated home page on client server

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
-
-import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
-
+import ThoughtForm from '../components/ThoughtForm';
 import FriendList from '../components/FriendList';
 
 import Auth from '../utils/auth';
@@ -21,7 +19,7 @@ const Home = () => {
       <div className="flex-row justify-space-between">
         {loggedIn && (
           <div className="col-12 mb-3">
-            <ThoughtForm /> 
+            <ThoughtForm />
           </div>
         )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
